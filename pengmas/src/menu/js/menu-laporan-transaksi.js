0c7c0e87457ee5b593d1d15473bba7b00ee94488
Shalrizky <144994306+Shalrizky@users.noreply.github.com>
2023-12-17 00:50:52 +0700

    Delete pengmas/src directory

--- pengmas/src/menu/js/menu-laporan-transaksi.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-   var buttonContainers = document.querySelectorAll(".button-container");
-   buttonContainers.forEach(function (container) {
-      container.addEventListener("click", function () {
-         var link = container.querySelector("a").getAttribute("href");
-         window.location.href = link;
-      });
-   });
-});
--- pengmas/src/menu/js/menu-utama.js
+++
@@ -1,7 +0,0 @@
- // Mengambil referensi elemen tombol logout
- var logoutBtn = document.getElementById("logoutBtn");
-
- // Menambahkan event listener untuk meng-handle klik tombol logout
- logoutBtn.addEventListener("click", function () {
-   window.location.href = "/src/loginRegister/login.html";
- });
