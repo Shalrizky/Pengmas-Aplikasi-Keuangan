6c453a0b42f37ff3dab9523921d351f405d2845b
Shalrizky <144994306+Shalrizky@users.noreply.github.com>
2024-01-24 00:33:10 +0700

    Delete src directory

--- src/shared/components/button-kembali-batal.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function()  {
-   const cancelButton = document.getElementById("cancelButton");
-
-   cancelButton.addEventListener("click", function() {
-      window.history.back();
-    });
-})
