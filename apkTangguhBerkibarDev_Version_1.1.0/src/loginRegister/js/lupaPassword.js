501b9d063ee2b4c31ef68911caf947aa7f7e3f19
Shalrizky <144994306+Shalrizky@users.noreply.github.com>
2024-01-24 17:28:48 +0700

    Add files via upload
    Ini aplikasi development untuk mengubah mengedit dll

---
+++ apkTangguhBerkibarDev_Version_1.1.0/src/loginRegister/js/lupaPassword.js
@@ -0,0 +1,117 @@
+// TODO: MERUBAH FLOW MENJADI CLEAN CODE SEPERTI PROFILE 
+// TODO: LINK RESET ADA SEDIKIT MISS DIMANA JIKA TERDAPAT LINK LAMA YANG SUDAH MELEBIHI BATAS WAKTU DAN JINKA LINK BARU DIKIRIM MAKA LINK LAMA BISA MENGAKSES FORMNYA SEHARUSNYA TIDAK DIPERBOLEHKAN HAL INI BERKAITAN DENGAN RESET PASSWORD 
+
+import { app } from "/src/init.js";
+import {
+  getDatabase,
+  ref,
+  update,
+  get,
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import {
+  getAuth,
+  sendPasswordResetEmail,
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const auth = getAuth(app);
+const db = getDatabase(app);
+
+// Function to show a Bootstrap alert
+function showBootstrapAlert(message, type = "danger") {
+  const alertContainer = document.getElementById("alertContainer");
+  alertContainer.innerHTML = `
+    <div class="alert alert-${type} alert-dismissible fade show" role="alert">
+      ${message}
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+  `;
+}
+
+// function validasi email input
+function validateEmail() {
+  const emailInputElement = document.querySelector("#inputEmail");
+  const invalidEmailFeedback = document.querySelector("#invalidEmailFeedback");
+  const emailInput = emailInputElement.value.trim();
+  emailInputElement.classList.remove("is-valid");
+  emailInputElement.classList.remove("is-invalid");
+  invalidEmailFeedback.textContent = "";
+
+  if (emailInput === "") {
+    invalidEmailFeedback.textContent = "Email Tidak Boleh Kosong";
+    emailInputElement.classList.add("is-invalid");
+    return false;
+  } else if (!isValidEmail(emailInput)) {
+    invalidEmailFeedback.textContent = "Format Email Tidak Sesuai";
+    emailInputElement.classList.add("is-invalid");
+    return false;
+  } else {
+    emailInputElement.classList.add("is-valid");
+    return true;
+  }
+}
+
+// fungsi menangani kiriman link email password
+async function handleEmailLink() {
+  const emailInput = document.getElementById("inputEmail").value;
+
+  if (!validateEmail()) {
+    return;
+  }
+
+  try {
+    const snapshot = await get(ref(db, "users"));
+    const users = snapshot.val();
+    const user = Object.values(users).find((user) => user.email === emailInput);
+
+    if (user) {
+      await sendPasswordResetEmail(auth, emailInput);
+      // timestamp reset password ke node pengguna berdasarkan UID
+      const userRef = ref(db, `users/${user.uid}`);
+      await update(userRef, {
+        lastPasswordResetTimestamp: Date.now(),
+      });
+
+      showBootstrapAlert(
+        "Link reset password telah dikirim ke email Anda, harap check notifikasi inbox/spam",
+        "success"
+      );
+    } else {
+      showBootstrapAlert("Email tidak terdaftar.");
+    }
+  } catch (error) {
+    console.error(
+      "Error in sending password reset email or updating timestamp:",
+      error
+    );
+  }
+}
+
+function isValidEmail(email) {
+  const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  return emailPattern.test(email);
+}
+
+function initializePage() {
+  const emailLinkForm = document.querySelector("#emailLinkForm");
+  const emailInput = document.querySelector("#inputEmail");
+
+  // Memanggil functuion email input
+  emailInput.addEventListener("input", validateEmail);
+
+  function lupaPwdBtnClicked() {
+    if (validateEmail()) {
+      handleEmailLink(emailInput.value);
+      emailInput.value = "";
+      emailInput.classList.remove("is-valid");
+    }
+  }
+
+  emailLinkForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    lupaPwdBtnClicked();
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  initializePage();
+});
